1ad77f84846d2347014e45cb3a50f9ae483d0ae8
Mirage_Panda <106939744+Mirage-Panda@users.noreply.github.com>
2025-01-06 21:20:53 +0800

    Smartened point creation

--- Manim/lorenz.py
+++ Manim/lorenz.py
@@ -32,17 +32,6 @@ class LorenzAttractor(ThreeDScene):  # Use ThreeDScene for 3D rotations
         # Time for evolution
         evolution_time = 20
 
-        # Generate points for two attractors with slightly different starting states
-        starting_state_1 = [10, 10, 10]
-        starting_state_2 = [10, 10, 10.001]  # Slight variation in the z-coordinate
-        starting_state_3 = [10, 10.001, 10]  # Slight variation in the z-coordinate
-        starting_state_4 = [10.001, 10, 10]  # Slight variation in the z-coordinate
-
-        points_1 = generate_lorenz_points(starting_state_1, t_max=evolution_time)
-        points_2 = generate_lorenz_points(starting_state_2, t_max=evolution_time)
-        points_3 = generate_lorenz_points(starting_state_3, t_max=evolution_time)
-        points_4 = generate_lorenz_points(starting_state_4, t_max=evolution_time)
-
         # Set up axes
         axes = ThreeDAxes(
             x_range=[-50, 50, 5],
@@ -51,93 +40,83 @@ class LorenzAttractor(ThreeDScene):  # Use ThreeDScene for 3D rotations
             axis_config={"color": GRAY, "stroke_width": 2, "include_tip": False},
         )
 
-        # Create paths for both attractors
-        attractor_path_1 = VMobject(color=BLUE, stroke_width=2)
-        attractor_path_1.set_points_smoothly([axes.c2p(*point) for point in points_1])
+        # Define parameters for the attractors
+        num_curves = 6  # Change to add more curves
+        epsilion = 1e-5  # Difference in set of curves
+        starting_states = [[10, 10, 10 + n * epsilion] for n in range(num_curves)]
+        colours = color_gradient([BLUE_E, BLUE_A], num_curves)
 
-        attractor_path_2 = VMobject(color=RED, stroke_width=2)
-        attractor_path_2.set_points_smoothly([axes.c2p(*point) for point in points_2])
+        # Generate points and create paths dynamically
+        paths = []
+        start_dots = []
 
-        attractor_path_3 = VMobject(color=RED, stroke_width=2)
-        attractor_path_3.set_points_smoothly([axes.c2p(*point) for point in points_3])
+        for i, state in enumerate(starting_states):
+            points = generate_lorenz_points(state, t_max=evolution_time)
+            path = VMobject(color=colours[i], stroke_width=2)
+            path.set_points_smoothly([axes.c2p(*point) for point in points])
+            paths.append(path)
 
-        attractor_path_4 = VMobject(color=RED, stroke_width=2)
-        attractor_path_4.set_points_smoothly([axes.c2p(*point) for point in points_4])
-
-        # Add starting points (dots) for both attractors
-        start_dot_1 = Dot3D(axes.c2p(*points_1[0]), color=BLUE, radius=0.05)
-        start_dot_2 = Dot3D(axes.c2p(*points_2[0]), color=RED, radius=0.05)
-        start_dot_3 = Dot3D(axes.c2p(*points_3[0]), color=RED, radius=0.05)
-        start_dot_4 = Dot3D(axes.c2p(*points_4[0]), color=RED, radius=0.05)
-
-        # Add updaters to the dots to follow the end of their respective curves
-        start_dot_1.add_updater(lambda dot: dot.move_to(attractor_path_1.get_end()))
-        start_dot_2.add_updater(lambda dot: dot.move_to(attractor_path_2.get_end()))
-        start_dot_3.add_updater(lambda dot: dot.move_to(attractor_path_3.get_end()))
-        start_dot_4.add_updater(lambda dot: dot.move_to(attractor_path_4.get_end()))
+            start_dot = Dot3D(axes.c2p(*points[0]), color=colours[i], radius=0.05)
+            start_dot.add_updater(lambda dot, path=path: dot.move_to(path.get_end()))
+            start_dots.append(start_dot)
 
         # Display Lorenz equations
-        # Define each equation separately
-        eq1 = MathTex(r"\frac{dx}{dt} = \sigma(y - x)", font_size=36)
-        eq2 = MathTex(r"\frac{dy}{dt} = x(\rho - z) - y", font_size=36)
-        eq3 = MathTex(r"\frac{dz}{dt} = xy - \beta z", font_size=36)
+        lorenz_title = MathTex(r"\text{The Lorenz System}", font_size=72)
+        lorenz_equation = MathTex(
+            R"""
+            \begin{aligned}
+            \frac{\mathrm{d} x}{\mathrm{~d} t} & =\sigma(y-x) \\
+            \frac{\mathrm{d} y}{\mathrm{~d} t} & =x(\rho-z)-y \\
+            \frac{\mathrm{d} z}{\mathrm{~d} t} & =x y-\beta z
+            \end{aligned}
+        """,
+            font_size=36,
+        )
+        lorenz_latex = VGroup(lorenz_title, lorenz_equation).arrange(
+            DOWN, aligned_edge=LEFT
+        )
+        lorenz_latex.move_to(UP * 1)
 
-        # Group the equations together and arrange them vertically
-        lorenz_equations = VGroup(eq1, eq2, eq3).arrange(DOWN, aligned_edge=LEFT)
-        lorenz_equations.move_to(UP * 1)
-
-        self.add_fixed_in_frame_mobjects(lorenz_equations)
+        self.add_fixed_in_frame_mobjects(lorenz_latex)
         self.wait(1)
-        self.play(FadeOut(lorenz_equations))
+        # self.play(FadeOut(lorenz_equations))
 
-        # Add axes and starting points to the scene
-        # self.add(axes)
-        self.play(FadeIn(axes), run_time=0.5)
+        # Add axes
+        self.play(
+            AnimationGroup(FadeOut(lorenz_latex), FadeIn(axes)),
+            run_time=1.5,
+            lag_ratio=0.5,
+        )
 
-        # Add rotation to the camera during the attractor animation
+        # Add rotation to the camera
         self.move_camera(
-            phi=60 * DEGREES,  # Initial elevation angle
-            theta=-45 * DEGREES,  # Initial azimuth angle
+            phi=60 * DEGREES,
+            theta=-45 * DEGREES,
             zoom=0.75,
             frame_center=(0, 0, 2),
         )
-        self.begin_ambient_camera_rotation(rate=0.05)  # Slow continuous rotation
+        self.begin_ambient_camera_rotation(rate=0.05)
 
-        # Animate both attractors
-        self.add(start_dot_2, start_dot_3, start_dot_4, start_dot_1)
+        # Animate attractors
+        self.add(*start_dots)
         self.play(
-            Create(attractor_path_2),
-            Create(attractor_path_3),
-            Create(attractor_path_4),
-            Create(attractor_path_1),
+            *[Create(path) for path in paths],
             run_time=evolution_time,
             rate_func=linear,
         )
 
-        # Stop updaters after the animation
-        start_dot_1.clear_updaters()
-        start_dot_2.clear_updaters()
-        start_dot_3.clear_updaters()
-        start_dot_4.clear_updaters()
+        # Stop updaters
+        for dot in start_dots:
+            dot.clear_updaters()
 
-        # Animation finished indicator
-        attractor_path_1.set_color(TEAL)
-        attractor_path_2.set_color(GREEN)
-        attractor_path_3.set_color(GREEN)
-        attractor_path_4.set_color(GREEN)
-        start_dot_1.set_color(TEAL)
-        start_dot_2.set_color(GREEN)
-        start_dot_3.set_color(GREEN)
-        start_dot_4.set_color(GREEN)
+        # Change colours to indicate completion
+        # for i, path in enumerate(paths):
+        #     path.set_color(TEAL)
+        #     start_dots[i].set_color(TEAL)
 
-        # Speed up camera
+        # Adjust camera speed
         self.stop_ambient_camera_rotation()
         self.begin_ambient_camera_rotation(rate=0.2)
         self.wait(0.1)
-        self.move_camera(
-            zoom=1,
-            frame_center=(0, 0, 3),
-        )
-
-        # Wait to observe the rotation
+        self.move_camera(zoom=1, frame_center=(0, 0, 3))
         self.wait(10)
