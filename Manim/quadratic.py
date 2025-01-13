511e8ce4ee674d8e0eacfe986fe4ca736fe6bb2a
Mirage_Panda <106939744+Mirage-Panda@users.noreply.github.com>
2025-01-13 16:27:08 +0800

    Coloured variables

--- Manim/quadratic.py
+++ Manim/quadratic.py
@@ -4,11 +4,41 @@ from manim import *
 class QuadraticFormula(Scene):
     def construct(self):
         equations = [
-            MathTex("ax^2 + bx + c = 0", font_size=72),
-            MathTex("4a^2x^2 + 4abx + 4ac = 0", font_size=72),
-            MathTex("4a^2x^2 + bx + b^2 = b^2 - 4ac", font_size=72),
-            MathTex("(2ax + b)^2 = b^2 - 4ac", font_size=72),
-            MathTex("2ax + b = \\pm \\sqrt{b^2 - 4ac}", font_size=72),
+            MathTex(
+                "ax^2 + bx + c = 0",
+                font_size=72,
+                substrings_to_isolate=["a", "b", "c", "x"],
+            ).set_color_by_tex_to_color_map(
+                {"x": GOLD, "a": RED, "b": BLUE, "c": GREEN}
+            ),
+            MathTex(
+                "4a^2x^2 + 4abx + 4ac = 0",
+                font_size=72,
+                substrings_to_isolate=["a", "b", "c", "x"],
+            ).set_color_by_tex_to_color_map(
+                {"x": GOLD, "a": RED, "b": BLUE, "c": GREEN}
+            ),
+            MathTex(
+                "4a^2x^2 + bx + b^2 = b^2 - 4ac",
+                font_size=72,
+                substrings_to_isolate=["a", "b", "c", "x"],
+            ).set_color_by_tex_to_color_map(
+                {"x": GOLD, "a": RED, "b": BLUE, "c": GREEN}
+            ),
+            MathTex(
+                "(2ax + b)^2 = b^2 - 4ac",
+                font_size=72,
+                substrings_to_isolate=["a", "b", "c", "x"],
+            ).set_color_by_tex_to_color_map(
+                {"x": GOLD, "a": RED, "b": BLUE, "c": GREEN}
+            ),
+            MathTex(
+                "2ax + b = \\pm \\sqrt{b^2 - 4ac}",
+                font_size=72,
+                substrings_to_isolate=["a", "b", "c", "x", "\\sqrt"],
+            ).set_color_by_tex_to_color_map(
+                {"x": GOLD, "a": RED, "b": BLUE, "c": GREEN, "\\sqrt": WHITE}
+            ),
             MathTex("x =\\frac{-b \\pm \\sqrt{b^2 - 4ac}}{2a}", font_size=72),
         ]
 
