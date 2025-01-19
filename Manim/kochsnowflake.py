c5a204a4241744c7fd0bae4f82658c529ec8b852
Mirage_Panda <106939744+Mirage-Panda@users.noreply.github.com>
2025-01-19 22:20:59 +0800

    Cleaned up

--- Manim/kochsnowflake.py
+++ Manim/kochsnowflake.py
@@ -3,36 +3,54 @@ from manim import *
 
 class KochSnowflake(Scene):
     def construct(self):
-        def KochCurve(n, length=5):
-            l = length / (3**n)
-            LineGroup = Line().set_length(l)
+        # Define the Koch curve function
+        def koch_curve(iterations, length=5):
+            # Calculate the initial segment length based on the number of iterations
+            segment_length = length / (3**iterations)
 
-            def NextLevel(LineGroup):
+            # Create the base line segment
+            line_group = Line().set_length(segment_length)
+
+            # Function to generate the next level of the Koch curve
+            def next_level(line_group):
                 return VGroup(
-                    *[LineGroup.copy().rotate(i) for i in [0, -PI / 3, PI / 3, 0]]
+                    *[line_group.copy().rotate(i) for i in [0, -PI / 3, PI / 3, 0]]
                 ).arrange(RIGHT, buff=0, aligned_edge=UP)
 
-            for _ in range(n):
-                LineGroup = NextLevel(LineGroup)
-            KC1 = (
+            # Recursively generate the Koch curve for the specified number of iterations
+            for _ in range(iterations):
+                line_group = next_level(line_group)
+
+            # Create the three parts of the Koch snowflake
+            part_1 = (
                 VMobject(stroke_width=2)
-                .set_points(LineGroup.get_all_points())
+                .set_points(line_group.get_all_points())
                 .set_color(YELLOW)
             )
-            KC2 = (
-                KC1.copy()
-                .rotate(2 * PI / 3, about_point=KC1.get_start())
+            part_2 = (
+                part_1.copy()
+                .rotate(2 * PI / 3, about_point=part_1.get_start())
                 .shift(length * RIGHT)
             )
-            KC3 = (
-                KC1.copy()
-                .rotate(-2 * PI / 3, about_point=KC1.get_end())
+            part_3 = (
+                part_1.copy()
+                .rotate(-2 * PI / 3, about_point=part_1.get_end())
                 .shift(length * LEFT)
             )
-            return VGroup(KC1, KC2, KC3)
+            return VGroup(part_1, part_2, part_3)
 
-        kc = KochCurve(0).to_edge(UP).shift(DOWN)
-        self.add(kc)
-        for i in range(1, 4):
-            self.play(kc.animate.become(KochCurve(i)).to_edge(UP).shift(DOWN))
-        self.wait(3)
+        # Initialize the Koch snowflake with 0 iterations (triangle)
+        koch_snowflake = koch_curve(0).to_edge(UP).shift(DOWN)
+
+        # Fade in the initial snowflake
+        self.play(FadeIn(koch_snowflake))
+
+        # Animate the snowflake through multiple iterations
+        evolution_iterations = 4
+        for i in range(1, evolution_iterations + 1):
+            self.play(
+                koch_snowflake.animate.become(koch_curve(i)).to_edge(UP).shift(DOWN)
+            )
+
+        # Wait before ending the animation
+        self.wait(1)
